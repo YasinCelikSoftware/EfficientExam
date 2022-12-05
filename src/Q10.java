1abfe28629f2f1016929e8cf74c2aced6059f077
YasinCelikSoftware <yasin.celik4@outlook.com>
2022-12-05 16:41:54 +0300

    Q10 is edited.

--- src/Q10.java
+++ src/Q10.java
@@ -3,28 +3,38 @@ public class Q10 {
         run();
     }
 
-    public static double findExponancial(int a){
+    public static double findExponancial(int a)
+    {
         double result = 1 + a;
         double tempResult;
-        for(int i = 2; i<2147483647; i++){
+
+        for(int i = 2; i<2147483647; i++)
+        {
             tempResult = result;
             result += (double)pow(a,i) / findFactorial(i);
+
             if((result - tempResult) < 0.1)
                 return result;
-
         }
+
         return result;
+
     }
 
-    public static int findFactorial(int a){
+    public static int findFactorial(int a)
+    {
         int result = 1;
-        for(int i = a; i > 1; i--){
+
+        for(int i = a; i > 1; i--)
+        {
             result *= i;
         }
+
         return result;
     }
 
-    public static int pow(int a, int b) {
+    public static int pow(int a, int b)
+    {
         int result = 1;
 
         for (int x = 0; x < b; x++)
@@ -33,14 +43,20 @@ public class Q10 {
         return result;
     }
 
-    public static void run(){
+    public static void display(double a)
+    {
+        System.out.printf("e^x = %.3f", a);
+    }
+
+    public static void run()
+    {
         java.util.Scanner kb = new java.util.Scanner(System.in);
+
         System.out.print("Please enter the x (e^x) : ");
         int x = kb.nextInt();
+
         display(findExponancial(x));
     }
 
-    public static void display(double a){
-        System.out.printf("e^x = %.3f", a);
-    }
+
 }
