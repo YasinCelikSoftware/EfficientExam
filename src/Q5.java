c3673ecd6046318fc304f0dc39f12eed50a14af8
YasinCelikSoftware <yasin.celik4@outlook.com>
2022-12-05 16:39:34 +0300

    Q5 is made with babylonian algorithm.

--- src/Q5.java
+++ src/Q5.java
@@ -1,6 +1,6 @@
 public class Q5 {
     public static void main(String[] args) {
-        findRootsOfEquation(1,2,2);
+        findRootsOfEquation(1,5,-6);
     }
 
     public static void findRootsOfEquation(int a, int b, int c){
@@ -8,10 +8,28 @@ public class Q5 {
         if(delta < 0)
             System.out.println("There is no roots for this equation.");
 
-        double root1 = (-b + java.lang.Math.sqrt(delta)) / 2 * a;
-        double root2 = (-b - java.lang.Math.sqrt(delta)) / 2 * a;
+        double root1 = (-b + sqrtBabylonian(delta)) / 2 * a;
+        double root2 = (-b - sqrtBabylonian(delta)) / 2 * a;
 
-        System.out.println(root1);
-        System.out.println(root2);
+        System.out.printf("%.2f%n", root1);
+        System.out.printf("%.2f", root2);
+    }
+
+    public static double sqrtBabylonian(double number) {
+
+        if(number == 0)
+            return 0;
+
+        double guess = number / 2;
+        double lastGuess;
+
+        do {
+            lastGuess = guess;
+            double r = number / guess;
+            guess = (guess + r) / 2;
+
+        } while ((lastGuess - guess) >= 0.01);
+
+        return guess;
     }
 }
